b04adc0f027f1f84db50f6a8a6306ecaec8cf8b7
Truman <gongcunji@gmail.com>
2019-03-12 15:11:16 +0800

    init

---
+++ precache-manifest.b9024d8bbbc26483518c85d26741797b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ec9094e82c29807af39b",
+    "url": "/static/js/main.9acd6e3d.chunk.js"
+  },
+  {
+    "revision": "b545e3a22aa71a55cd8d",
+    "url": "/static/js/2.681dd139.chunk.js"
+  },
+  {
+    "revision": "ec9094e82c29807af39b",
+    "url": "/static/css/main.ff255c0e.chunk.css"
+  },
+  {
+    "revision": "b545e3a22aa71a55cd8d",
+    "url": "/static/css/2.ed734471.chunk.css"
+  },
+  {
+    "revision": "f1e9b209825bfc2c22fa5df729f088ea",
+    "url": "/index.html"
+  }
+];
